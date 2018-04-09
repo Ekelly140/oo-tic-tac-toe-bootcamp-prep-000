25afb7cc31e96ca255b10afce546ba3eeb2c6539
ERIC KELLY <eakelly220@gmail.com>
2018-04-09 23:21:24 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,14 +10,14 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
 end
  
  def position_taken?(@board,@index)
-   if (board[@index] == "X" || board[@index] == "O")
+   if (@board[@index] == "X" || @board[@index] == "O")
      return true 
    else 
      return false
    end
  end
 
-  def input_to_index(index)
+  def input_to_index(@index)
    @index = index.to_i - 1
   end 
 
