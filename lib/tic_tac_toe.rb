e2486fa246236c1096e03376d4aee30269b7968e
ERIC KELLY <eakelly220@gmail.com>
2018-04-09 23:34:59 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,7 +24,7 @@ end
  end
  
   def valid_move?(board, index)
-  if   index.between?(0,8) && (board[index] == " " || board[index] == "")
+  if   @index.between?(0,8) && (@board[@index] == " " || @board[@index] == "")
   return true 
   else 
   return false
