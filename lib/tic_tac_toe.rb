5ae68c0370340660f62a8069a83676bd0a35e9d5
ERIC KELLY <eakelly220@gmail.com>
2018-04-09 23:37:19 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,7 +15,7 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
   @board[@index.to_i] = token
 end
 
- def position_taken?(index)
+ def position_taken?(board, index)
    if (@board[@index] == "X" || @board[@index] == "O")
      return true 
    else 
