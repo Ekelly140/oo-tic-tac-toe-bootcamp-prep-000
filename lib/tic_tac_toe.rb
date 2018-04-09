9d84a2cdb3d095a9fe56ca3879771c569eb2d933
ERIC KELLY <eakelly220@gmail.com>
2018-04-09 23:33:21 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,7 +12,7 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
   end 
   
    def move(index, token = "X") 
-  board[@index.to_i] = char
+  @board[@index.to_i] = char
 end
 
  def position_taken?(board, index)
