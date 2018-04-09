787444c39401543fc5504e1c9dc801fc15cd232f
ERIC KELLY <eakelly220@gmail.com>
2018-04-09 23:01:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,6 +3,6 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
 
-WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[2,4,7],[2,5,6],[0,4,8][3,4,6]
+WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[2,4,7],[2,5,6],[0,4,8],[3,4,6]
 
 end 
